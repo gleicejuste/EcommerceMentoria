0b1491f98b7e64cc72282533c5a063dd2ea2f4c6
Gleice Juste <glejuste@hotmail.com>
2022-11-07 09:19:29 -0300

    //removendo comentarios desnecessários

--- EM.Domain/Util/Util.cs
+++ EM.Domain/Util/Util.cs
@@ -24,27 +24,5 @@ namespace EM.Domain.Util
             Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SaltSize, Pbkdf2SubkeyLength);
             return Convert.ToBase64String(outputBytes);
         }
-
-
-//        Console.Write("Enter a password: ");
-//        string password = Console.ReadLine();
-
-//        // generate a 128-bit salt using a secure PRNG
-//        byte[] salt = new byte[128 / 8];
-//        using (var rng = RandomNumberGenerator.Create())
-//        {
-//            rng.GetBytes(salt);
-//        }
-//Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
-
-//// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
-//string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-//    password: password,
-//    salt: salt,
-//    prf: KeyDerivationPrf.HMACSHA1,
-//    iterationCount: 10000,
-//    numBytesRequested: 256 / 8));
-//Console.WriteLine($"Hashed: {hashed}");
-
     }
 }
